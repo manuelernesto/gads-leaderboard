663bae495aef2f52952c584471b95dd9baed0a5c
manuelernesto <manuelernesto@outlook.com>
2020-09-10 12:57:44 +0100

    refactoring code

--- app/src/main/java/io/github/manuelernesto/gadsleaderboard/ui/MainActivity.kt
+++ app/src/main/java/io/github/manuelernesto/gadsleaderboard/ui/MainActivity.kt
@@ -13,22 +13,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var hourFragment: HourFragment
-    private lateinit var skiFragment: SkillFragment
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        hourFragment = HourFragment()
-        skiFragment = SkillFragment()
-
-
         tab_layout.setupWithViewPager(view_pager)
-        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, 0)
-        viewPagerAdapter.addFragment(hourFragment, "Learning Leaders")
-        viewPagerAdapter.addFragment(skiFragment, "Skill IQ Leaders")
+
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, 0).also {
+            it.addFragment(HourFragment(), "Learning Leaders")
+            it.addFragment(SkillFragment(), "Skill IQ Leaders")
+        }
+
         view_pager.adapter = viewPagerAdapter
 
         btn_submit.setOnClickListener {
