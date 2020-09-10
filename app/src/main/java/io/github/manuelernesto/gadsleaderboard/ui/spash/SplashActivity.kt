8fca1e7b6a62174488cb9c5119194451e12c5308
manuelernesto <manuelernesto@outlook.com>
2020-09-11 00:00:22 +0100

    add SplashActivity.kt

---
+++ app/src/main/java/io/github/manuelernesto/gadsleaderboard/ui/spash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package io.github.manuelernesto.gadsleaderboard.ui.spash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.manuelernesto.gadsleaderboard.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spash)
+    }
+}
