e07797d529af9f86d1737dea58cead28a7a4ace2
manuelernesto <manuelernesto@outlook.com>
2020-09-10 23:51:59 +0100

    creating the SubmitActivity.kt

--- app/src/main/java/io/github/manuelernesto/gadsleaderboard/util/constants.kt
+++ app/src/main/java/io/github/manuelernesto/gadsleaderboard/util/constants.kt
@@ -7,3 +7,8 @@ const val SKILLIQ_URL = "skilliq"
 const val HOUR_URL = "hours"
 
 const val FORM_ID = "1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse"
+
+const val ENTRY_EMAIL = "entry.1824927963"
+const val ENTRY_FIRST_NAME = "entry.1877115667"
+const val ENTRY_LAST_NAME = "entry.2006916086"
+const val ENTRY_LINK = "entry.284483984"
